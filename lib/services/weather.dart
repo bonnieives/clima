a15e96053c435e49619b8466efeb490a27d881f3
Bonnie Ives de Castro Nunes <120677106+bonnieives@users.noreply.github.com>
2024-02-25 17:12:31 -0500

    Update weather.dart

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,7 +1,7 @@
 import 'package:clima/services/location_provider.dart';
 import 'package:clima/services/networking.dart';
 
-const apiKey = 'f3aefad0326d799d8058e2e858720b2b';
+const apiKey = '';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
